13f88f2a968b0de7e61a0f60fe26f0a27f4fd9b2
Matias Bella <matias.bella@hotmail.com>
2023-09-19 17:28:15 -0300

    config

---
+++ nvim/lua/matias/set.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.rnu = true
+vim.opt.tabstop = 6
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 6
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+
+vim.opt.smartindent = true
+vim.cmd("set breakindent")
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
